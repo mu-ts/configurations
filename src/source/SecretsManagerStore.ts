90d6a7e38948e6312ca69c14df955d5963f3d093
Matt Filion <matt.filion@gmail.com>
2020-04-22 11:54:47 -0700

    2.1.1
    More unit tests to handle possible failures in SecretsManager while it loads.

--- src/source/SecretsManagerStore.ts
+++ src/source/SecretsManagerStore.ts
@@ -15,7 +15,7 @@ export class SecretsManagerStore implements Source {
   );
   private readonly logger: Logger;
   private readonly storeName: string;
-  private readonly region: string | undefined;
+  public readonly region: string | undefined;
   private readonly secureCache: SecureCache;
   private readonly secretsManager: SecretsManager;
 
@@ -23,11 +23,15 @@ export class SecretsManagerStore implements Source {
     this.logger = LoggerService.named({ name: this.constructor.name, adornments: { '@mu-ts': 'configurations' } });
 
     this.storeName = storeName;
-    this.region = region;
+    this.region = region || process.env.AWS_REGION || process.env.REGION || 'us-east-1';
     this.secureCache = new SecureCache();
+    this.logger.debug('Creating secretsManager client', {
+      region: this.region,
+    });
+
     this.secretsManager = new SecretsManager({
       apiVersion: '2017-10-17',
-      region: this.region || process.env.AWS_REGION || process.env.REGION || 'us-east-1',
+      region: this.region,
     });
 
     this.logger.info('init()');
@@ -73,16 +77,33 @@ export class SecretsManagerStore implements Source {
       };
 
       this.logger.debug('load()', 'requesting secrets', { parameters });
+
+      // In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+      // See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+      // We rethrow the exception by default.
       const response: GetSecretValueResponse = await this.secretsManager.getSecretValue(parameters).promise();
 
       this.logger.debug('load()', 'done');
 
-      if (!response.SecretString) {
+      let values: { [key: string]: { [key: string]: string } | string };
+
+      if ('SecretString' in response) {
+        this.logger.debug('load()', 'Looking within "SecretString"');
+
+        if (response.SecretString) values = JSON.parse(response.SecretString);
+      } else {
+        this.logger.debug('load()', 'Looking within "SecretBinary"');
+
+        const buff: Buffer = Buffer.from(response.SecretBinary as Buffer);
+        const decodedBinarySecret: string = buff.toString('ascii');
+
+        if (decodedBinarySecret) values = JSON.parse(decodedBinarySecret);
+      }
+
+      if (!values) {
         this.logger.error('load()', 'There is no secret stringin the secret store.');
         throw Error(`There is no secret string in the secret store named ${this.storeName}.`);
       }
-
-      const values: { [key: string]: { [key: string]: string } | string } = JSON.parse(response.SecretString);
 
       this.logger.debug('load()', 'values parsed from secrets.');
 
@@ -95,16 +116,45 @@ export class SecretsManagerStore implements Source {
       }
     } catch (error) {
       /**
-       * When a secret name is not properly defined, we cannot 'recover' from it so
-       * throw a hard failure.
-       */
-      if (error.message.startsWith('There is no secret')) throw error;
-      /**
        * There are plenty of conditions around connectivity where secrets re-loading
        * will recover the configuration, so this allows the exception to log and continue.
        * In the future, we will be more explicit about these failures.
        */
-      this.logger.error('load()', 'Exception raised while trying to load secrets.', error);
+      if (error.code === 'DecryptionFailureException') {
+        // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        this.logger.warn('load()', 'Exception raised while trying to load secrets.', error);
+        /**
+         * Give the system a chance to self heal.
+         */
+        return undefined;
+      } else if (error.code === 'InternalServiceErrorException') {
+        // An error occurred on the server side.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        this.logger.warn('load()', 'Exception raised while trying to load secrets.', error);
+        /**
+         * Give the system a chance to self heal.
+         */
+        return undefined;
+      } else if (error.code === 'InvalidParameterException') {
+        // You provided an invalid value for a parameter.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        this.logger.error('load()', 'Exception raised while trying to load secrets.', error);
+        throw error;
+      } else if (error.code === 'InvalidRequestException') {
+        // You provided a parameter value that is not valid for the current state of the resource.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        this.logger.error('load()', 'Exception raised while trying to load secrets.', error);
+        throw error;
+      } else if (error.code === 'ResourceNotFoundException') {
+        // We can't find the resource that you asked for.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        this.logger.error('load()', 'Exception raised while trying to load secrets.', error);
+        throw error;
+      } else {
+        this.logger.error('load()', 'Exception raised while trying to load secrets.', error);
+        throw error;
+      }
     }
   }
 }
